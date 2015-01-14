20507fe6be3242b981e05863cbf3d7d4329cafbb
Mike Reining <emailmike@gmail.com>
2015-01-13 17:19:57 -0800

    Added Player Names
    added function that requests Player Names before game is played

--- 2PlayerMathGame.playground/section-1.swift
+++ 2PlayerMathGame.playground/section-1.swift
@@ -4,8 +4,6 @@ import Cocoa
 
 // MARK: Setup game variables
 
-var player1Lives = 3
-var player2Lives = 3
 var player1Name = "Player 1"
 var player2Name = "Player 2"
 var gameRound = 1
@@ -35,88 +33,109 @@ func generateRandomNumberUpTo40() -> Int {
 }
 
 
-// MARK: Play Game
+// MARK: Play Game Logic
 
-while player1Lives > 0 && player2Lives > 0 {
-    println(" ")
-    println("This is round: \(gameRound)")
+func playGame (#firstPlayerName: String, #secondPlayerName: String) {
+    player1Name = firstPlayerName
+    player2Name = secondPlayerName
+    var player1Lives = 3
+    var player2Lives = 3
+    println("STARTING NEW MATH GAME!")
+    println("PLAYER 1 is: \(player1Name)")
+    println("PLAYER 2 is: \(player2Name)")
     
-    // Player 1 takes turn implemented with Better Math (i.e. random math function)
-
-    var randomNumber1 = generateRandomNumberUpTo20()
-    var randomNumber2 = generateRandomNumberUpTo20()
-    var randomMathType1 = generateRandomNumberUpTo4()
-    var player1Answer = generateRandomNumberUpTo20()
-    
-    switch randomMathType1 {
-    case 1:
-        println("\(player1Name): What is \(randomNumber1) plus \(randomNumber2)?")
-        var correctAnswer = randomNumber1 + randomNumber2
-        println("Player 1 said: \(player1Answer)")
-        println("The correct answer is: \(correctAnswer)")
-        if player1Answer != correctAnswer {
-            player1Lives -= 1
-            println("CURRENT SCORE: Player 1 lives: \(player1Lives) and Player 2 lives: \(player2Lives)")
+    while player1Lives > 0 && player2Lives > 0 {
+        println(" ")
+        println("This is round: \(gameRound)")
+        
+        // Player 1 takes turn implemented with Better Math (i.e. random math function)
+        
+        var randomNumber1 = generateRandomNumberUpTo20()
+        var randomNumber2 = generateRandomNumberUpTo20()
+        var randomMathType1 = generateRandomNumberUpTo4()
+        var player1Answer = generateRandomNumberUpTo20()
+        
+        switch randomMathType1 {
+        case 1:
+            println("\(player1Name): What is \(randomNumber1) plus \(randomNumber2)?")
+            var correctAnswer = randomNumber1 + randomNumber2
+            println("\(player1Name) said: \(player1Answer)")
+            println("The correct answer is: \(correctAnswer)")
+            if player1Answer != correctAnswer {
+                player1Lives -= 1
+                println("CURRENT SCORE: \(player1Name) lives: \(player1Lives) and \(player2Name) lives: \(player2Lives)")
+            }
+            
+        case 2:
+            println("\(player1Name): What is \(randomNumber1) minus \(randomNumber2)?")
+            var correctAnswer = randomNumber1 - randomNumber2
+            println("\(player1Name) said: \(player1Answer)")
+            println("The correct answer is: \(correctAnswer)")
+            if player1Answer != correctAnswer {
+                player1Lives -= 1
+                println("CURRENT SCORE: \(player1Name) lives: \(player1Lives) and \(player2Name) lives: \(player2Lives)")
+            }
+            
+        case 3:
+            println("\(player1Name): What is \(randomNumber1) times \(randomNumber2)?")
+            var correctAnswer = randomNumber1 * randomNumber2
+            println("\(player1Name) said: \(player1Answer)")
+            println("The correct answer is: \(correctAnswer)")
+            if player1Answer != correctAnswer {
+                player1Lives -= 1
+                println("CURRENT SCORE: \(player1Name) lives: \(player1Lives) and \(player2Name) lives: \(player2Lives)")
+            }
+            
+        case 4:
+            println("\(player1Name): What is \(randomNumber1) divided by \(randomNumber2)?")
+            var correctAnswer = Float(randomNumber1) / Float(randomNumber2)
+            println("\(player1Name) said: \(player1Answer)")
+            println("The correct answer is: \(correctAnswer)")
+            if Float(player1Answer) != correctAnswer {
+                player1Lives -= 1
+                println("CURRENT SCORE: \(player1Name) lives: \(player1Lives) and \(player2Name) lives: \(player2Lives)")
+            }
+            
+        default:
+            println("something is wrong")
         }
         
-    case 2:
-        println("\(player1Name): What is \(randomNumber1) minus \(randomNumber2)?")
-        var correctAnswer = randomNumber1 - randomNumber2
-        println("Player 1 said: \(player1Answer)")
-        println("The correct answer is: \(correctAnswer)")
-        if player1Answer != correctAnswer {
-            player1Lives -= 1
-            println("CURRENT SCORE: Player 1 lives: \(player1Lives) and Player 2 lives: \(player2Lives)")
+        if player1Lives == 0 {
+            println("Player 2: \(player2Name) Wins!!!")
+            break
         }
         
-    case 3:
-        println("\(player1Name): What is \(randomNumber1) times \(randomNumber2)?")
-        var correctAnswer = randomNumber1 * randomNumber2
-        println("Player 1 said: \(player1Answer)")
-        println("The correct answer is: \(correctAnswer)")
-        if player1Answer != correctAnswer {
-            player1Lives -= 1
-            println("CURRENT SCORE: Player 1 lives: \(player1Lives) and Player 2 lives: \(player2Lives)")
+        // Player 2 Takes Turn - Simple Math - Addition question only for Player 2
+        
+        var randomNumber3 = generateRandomNumberUpTo20()
+        var randomNumber4 = generateRandomNumberUpTo20()
+        
+        println ("\(player2Name): What is \(randomNumber3) plus \(randomNumber4)?")
+        var correctAnswerPlayer2 = randomNumber3 + randomNumber4
+        var player2Answer = generateRandomNumberUpTo40()
+        
+        println("\(player2Name) said: \(player2Answer)")
+        println("The correct answer is: \(correctAnswerPlayer2)")
+        if player2Answer != correctAnswerPlayer2 {
+            player2Lives -= 1
+            println("CURRENT SCORE: \(player1Name) lives: \(player1Lives) and \(player2Name) lives: \(player2Lives)")
+        }
+        if player2Lives == 0 {
+            println("Player 1: \(player1Name) Wins!!!")
+            break
         }
         
-    case 4:
-        println("\(player1Name): What is \(randomNumber1) divided by \(randomNumber2)?")
-        var correctAnswer = Float(randomNumber1) / Float(randomNumber2)
-        println("Player 1 said: \(player1Answer)")
-        println("The correct answer is: \(correctAnswer)")
-        if Float(player1Answer) != correctAnswer {
-            player1Lives -= 1
-            println("CURRENT SCORE: Player 1 lives: \(player1Lives) and Player 2 lives: \(player2Lives)")
-        }
-        
-    default:
-        println("something is wrong")
+        gameRound += 1
     }
-    
-    if player1Lives == 0 {
-        println("Player 2 Wins!!!")
-        break
-    }
-    
-    // Player 2 Takes Turn - Simple Math - Addition question only for Player 2
-    
-    var randomNumber3 = generateRandomNumberUpTo20()
-    var randomNumber4 = generateRandomNumberUpTo20()
-    
-    println ("\(player2Name): What is \(randomNumber3) plus \(randomNumber4)?")
-    var correctAnswerPlayer2 = randomNumber3 + randomNumber4
-    var player2Answer = generateRandomNumberUpTo40()
-
-    println("Player 2 said: \(player2Answer)")
-    println("The correct answer is: \(correctAnswerPlayer2)")
-    if player2Answer != correctAnswerPlayer2 {
-        player2Lives -= 1
-        println("CURRENT SCORE: Player 1 lives: \(player1Lives) and Player 2 lives: \(player2Lives)")
-    }
-    if player2Lives == 0 {
-        println("Player 1 Wins!!!")
-        break
-    }
-    
-    gameRound += 1
+    println("")
+    println("GAME OVER")
+    println("")
 }
+
+// MARK: Play Game with different player names
+
+
+playGame(firstPlayerName: "Mike", secondPlayerName: "John")
+
+playGame(firstPlayerName: "Sarah", secondPlayerName: "Dave")
+
